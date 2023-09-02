de0960b0b8ee3b611be65ad3c0446170e6f443b2
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2023-09-02 11:19:46 +1000

    Rollup to do commonjs conversion

---
+++ packages/wireyui-core/rollup.config.mjs
@@ -0,0 +1,11 @@
+export default {
+    input: [
+        './build/index.js',
+        './build/jsx-runtime.js',
+
+    ],
+    output: {
+        format: 'es',
+        dir: './build/cjs'
+    },
+  };
---
+++ packages/wireyui-web/rollup.config.mjs
@@ -0,0 +1,10 @@
+export default {
+    input: [
+        './build/index.js',
+
+    ],
+    output: {
+        format: 'es',
+        dir: './build/cjs'
+    },
+  };

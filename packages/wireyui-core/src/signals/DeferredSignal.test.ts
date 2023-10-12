6dc3f1ddfa6bbfb5d5b1a44451bad7375c65b628
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2023-10-12 23:13:02 +1100

    More deferred testing

--- packages/wireyui-core/src/signals/DeferredSignal.test.ts
+++ packages/wireyui-core/src/signals/DeferredSignal.test.ts
@@ -1,7 +1,22 @@
 import { $mutable } from "./MutableValueSignal.js";
 import { $deferred } from "./DeferredSignal.js";
 
-it('DeferredSignal updated later', () => {
+it('DeferredSignal updated after microtask (default deferral mechanism)', async () => {
+    const mutable = $mutable(1);
+    const deferred = $deferred(mutable);
+
+    expect(deferred.value).toBe(1);
+
+    mutable.value = 2;
+
+    expect(deferred.value).toBe(1);
+
+    await new Promise<void>(resolve => queueMicrotask(() => resolve()))
+
+    expect(deferred.value).toBe(2);
+});
+
+it('DeferredSignal updated later (alternate deferral callback)', () => {
     const queue: (() => void)[] = [];
 
     const mutable = $mutable(1);

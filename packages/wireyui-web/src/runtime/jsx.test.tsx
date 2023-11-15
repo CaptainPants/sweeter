95797d234fa9bcbb08604f2d26a7d75ba2079eb5
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2023-11-15 16:45:09 +1100

    More unit test

--- packages/wireyui-web/src/runtime/jsx.test.tsx
+++ packages/wireyui-web/src/runtime/jsx.test.tsx
@@ -1,8 +1,10 @@
-import type { TypeMatchAssert } from "@captainpants/wireyui-core";
-import { jsx } from "./jsx.js";
+/* @jsxImportSource ../. */
 
-it('basic div creates with props', () => {
-    const div = jsx('div', { children: ["test", "content"], class: 'test' });
+import type { TypeMatchAssert } from '@captainpants/wireyui-core';
+import { jsx } from './jsx.js';
+
+it('basic div creates with props (function syntax)', () => {
+    const div = jsx('div', { children: ['test', 'content'], class: 'test' });
 
     // Confirm the result is a div, its nice to make sure that jsx() gives strongly typed results.
     const _typeAssert1: TypeMatchAssert<HTMLDivElement, typeof div> = true;
@@ -18,4 +20,32 @@ it('basic div creates with props', () => {
 
     expect(div.childNodes[1]).toBeInstanceOf(Text);
     expect((div.childNodes[1] as Text).textContent).toStrictEqual('content');
-})
+});
+
+it('basic div creates with props (using jsx syntax)', () => {
+    const div = (
+        <div class={'test'}>
+            {'test'}
+            {'content'}
+        </div>
+    );
+
+    // jsx always returns the opaque JSX.Element type
+    const _typeAssert1: TypeMatchAssert<JSX.Element, typeof div> = true;
+
+    expect(div).toBeInstanceOf(HTMLDivElement);
+
+    if (!(div instanceof HTMLDivElement)) {
+        throw new Error('Should be a div');
+    }
+
+    expect(div.className).toStrictEqual('test');
+
+    expect(div.childNodes.length).toStrictEqual(2);
+
+    expect(div.childNodes[0]).toBeInstanceOf(Text);
+    expect((div.childNodes[0] as Text).textContent).toStrictEqual('test');
+
+    expect(div.childNodes[1]).toBeInstanceOf(Text);
+    expect((div.childNodes[1] as Text).textContent).toStrictEqual('content');
+});

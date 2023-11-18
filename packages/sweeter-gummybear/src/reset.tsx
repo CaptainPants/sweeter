ab6561ecb4eaf40fe6c873cdb645dc8e6f4ef2c6
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2023-11-18 13:37:27 +1100

    More on reset.

--- packages/sweeter-gummybear/src/reset.tsx
+++ packages/sweeter-gummybear/src/reset.tsx
@@ -3,9 +3,11 @@ import { GlobalCssStylesheet } from '@captainpants/sweeter-web';
 export const reset = new GlobalCssStylesheet({
     id: 'reset',
     content: `
-        input, textarea, td, th {
+        select, option, input, textarea, tabel, thead, tbody, tr, td, th {
             font-family: inherit;
             font-size: inherit;
         }
     `,
+    // Pre-processor not required
+    preprocess: false,
 });

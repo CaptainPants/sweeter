f518e3bc01e58e06e60703474140cd977b17a269
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2023-11-19 14:18:00 +1100

    Implemented simple path for ForEach.

--- packages/sweeter-core/src/components/ForEach.ts
+++ packages/sweeter-core/src/components/ForEach.ts
@@ -1,4 +1,4 @@
-import { getRuntime } from '../index.js';
+import { $calc, $mutable, $val, getRuntime, isSignal } from '../index.js';
 import { type Signal } from '../signals/types.js';
 import {
     type MightBeSignal,
@@ -19,6 +19,17 @@ export function ForEach<T>(
     { items, renderItem }: ForEachProps<T>,
     init: ComponentInit,
 ): JSX.Element {
+    if (!isSignal(items)) {
+        // constant array, we can skip a lot of voodoo - the $calc is just because renderItem could be a signal
+        return $calc(() =>
+            items.map((x, i) => $val(renderItem)($mutable(x), i)),
+        );
+    }
+
+    // items is a signal, we need to keep track of a signal for every item
+    // including if it changes lengths to dispose/orphan signals that no longer
+    // point to a valid index, and add new signals when necessary.
+
     throw new TypeError('Not implemented');
 }
 

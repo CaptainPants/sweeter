7eaeb9539afd2393f1b0d61f99ab13f264b0b41b
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2023-11-27 17:48:49 +1100

    removeSelfAndLaterSiblings tests

---
+++ packages/sweeter-web/src/runtime/internal/utility/removeSelfAndLaterSiblings.test.ts
@@ -0,0 +1,25 @@
+import { removeSelfAndLaterSiblings } from "./removeSelfAndLaterSiblings.js";
+
+it('test', () => {
+    const item1 = document.createElement('div');
+    item1.textContent = 'item-1';
+    const item2 = document.createElement('div');
+    item2.textContent = 'item-2';
+    const item3 = document.createElement('div');
+    item3.textContent = 'item-3';
+
+    const parent = document.createElement('div');
+    parent.appendChild(item1);
+    parent.appendChild(item2);
+    parent.appendChild(item3);
+
+    removeSelfAndLaterSiblings(item2, () => {
+        
+    });
+
+    expect(item1.parentNode).toStrictEqual(null);
+    expect(item2.parentNode).toStrictEqual(parent);
+    expect(item3.parentNode).toStrictEqual(parent);
+
+    expect(item3.innerHTML).toStrictEqual('<div><div>item-1</div></div>');
+})

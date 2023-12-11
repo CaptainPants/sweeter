e19d6ce701e86e8740fe95fbf7a9eb6ab05ce784
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2023-12-11 17:14:35 +1100

    Switch to manually created object so we can add doc comments.

--- packages/sweeter-gummybear/src/stylesheets/tags.ts
+++ packages/sweeter-gummybear/src/stylesheets/tags.ts
@@ -1,10 +1,18 @@
 import { GlobalCssClass } from '@captainpants/sweeter-web';
-import { createConstantMap } from '../internal/createConstantMap.js';
 
-export const tags = createConstantMap(
-    ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light'],
-    (tag) =>
-        new GlobalCssClass({
-            className: tag,
-        }),
-);
+export const tags = {
+    primary: new GlobalCssClass({ className: 'primary' }),
+    secondary: new GlobalCssClass({ className: 'secondary' }),
+    success: new GlobalCssClass({ className: 'success' }),
+    danger: new GlobalCssClass({ className: 'danger' }),
+    warning: new GlobalCssClass({ className: 'warning' }),
+    info: new GlobalCssClass({ className: 'info' }),
+    light: new GlobalCssClass({ className: 'light' }),
+    
+    outline: new GlobalCssClass({ className: 'outline' }),
+
+    large: new GlobalCssClass({ className: 'large' }),
+    small: new GlobalCssClass({ className: 'small' }),
+
+    disabled: new GlobalCssClass({ className: 'disabled' }),
+}

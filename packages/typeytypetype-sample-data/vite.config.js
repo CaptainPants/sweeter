936db5106e4212d1c2d9a6068c70e3d1f638e2dc
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2024-01-05 16:13:29 +1100

    Migrated in typeytypetype-example-data

---
+++ packages/typeytypetype-sample-data/vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+    build: {
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            // eslint-disable-next-line no-undef
+            entry: resolve(__dirname, './src/index.ts'),
+            fileName: 'index',
+            name: 'index',
+            formats: ['es', 'cjs'],
+        },
+        outDir: 'build',
+        minify: false,
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: [],
+        },
+    },
+    plugins: [dts({ clearPureImport: false })]
+});

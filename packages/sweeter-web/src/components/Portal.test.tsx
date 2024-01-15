eccc170c8dfb15aadb21102757810a30b083b1fc
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2024-01-15 11:04:26 +1100

    Portal test fixed to remove host elements. Additional test for css class references.

--- packages/sweeter-web/src/components/Portal.test.tsx
+++ packages/sweeter-web/src/components/Portal.test.tsx
@@ -8,30 +8,37 @@ import {
 } from '@captainpants/sweeter-core';
 import { testRender } from '../test/testRender.js';
 import {
+    GlobalCssClass,
     GlobalCssStylesheet,
     IncludeStylesheet,
     stylesheet,
 } from '../index.js';
 
-// it('Portal element content is added', () => {
-//     const target = document.createElement('div');
+afterEach(() => {
+    document.getElementsByTagName('html')[0]!.innerHTML = ''; 
+});
 
-//     const { dispose, getHTML } = testRender(() => (
-//         <Portal target={target}>
-//             <div>CONTENT</div>
-//         </Portal>
-//     ));
+it('Portal element content is added', () => {
+    const target = document.createElement('div');
+    target.id = 'host1';
 
-//     // The contents of our in-tree render should be empty, but will have hook placeholders
-//     expect(getHTML()).toMatchSnapshot();
+    const { dispose, getHTML } = testRender(() => (
+        <Portal target={target}>
+            <div>CONTENT</div>
+        </Portal>
+    ));
 
-//     expect(target.outerHTML).toMatchSnapshot();
+    // The contents of our in-tree render should be empty, but will have hook placeholders
+    expect(getHTML()).toMatchSnapshot();
 
-//     dispose();
+    expect(target.outerHTML).toMatchSnapshot();
 
-//     // Should have been cleared
-//     expect(target.outerHTML).toMatchSnapshot();
-// });
+    dispose();
+
+    // Should have been cleared
+    expect(target.outerHTML).toMatchSnapshot();
+    target.remove();
+});
 
 it('Portal elements unmount correctly', () => {
     const shouldMount = $mutable(true);
@@ -61,7 +68,7 @@ it('Portal elements unmount correctly', () => {
     }
 
     const target = document.createElement('div');
-    target.id = 'host';
+    target.id = 'host2';
     document.body.append(target);
 
     const { dispose } = testRender(() => (
@@ -110,4 +117,86 @@ it('Portal elements unmount correctly', () => {
 
     expect(document.head.outerHTML).toMatchSnapshot();
     expect(document.body.outerHTML).toMatchSnapshot();
+
+    target.remove();
+});
+
+it('Portal elements unmount correctly (css class ref)', () => {
+    const shouldMount = $mutable(true);
+
+    const style = new GlobalCssClass({
+        className: 'name',
+        content: stylesheet`
+            div {
+                color: red;
+            }
+        `,
+    });
+
+    let mounted = false;
+
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    function TestComponent(props: {}, init: ComponentInit) {
+        init.onMount(() => {
+            mounted = true;
+
+            return () => {
+                mounted = false;
+            };
+        });
+
+        return <></>;
+    }
+
+    const target = document.createElement('div');
+    target.id = 'host3';
+    document.body.append(target);
+
+    const { dispose } = testRender(() => (
+        <Dynamic<boolean> value={shouldMount}>
+            {(visible) =>
+                visible ? (
+                    <>
+                        MOUNTED
+                        <Portal target={target}>
+                            <div class={[style]}>MOUNTED</div>
+                        </Portal>
+                        <TestComponent />
+                    </>
+                ) : (
+                    'NOT MOUNTED'
+                )
+            }
+        </Dynamic>
+    ));
+
+    shouldMount.value = true;
+    expect(mounted).toStrictEqual(true);
+
+    expect(document.head.outerHTML).toMatchSnapshot();
+    expect(document.body.outerHTML).toMatchSnapshot();
+
+    shouldMount.value = false;
+    expect(mounted).toStrictEqual(false);
+
+    // Should have been cleared
+    expect(document.head.outerHTML).toMatchSnapshot();
+    expect(document.body.outerHTML).toMatchSnapshot();
+
+    shouldMount.value = true;
+    expect(mounted).toStrictEqual(true);
+
+    // Everything should be back
+    expect(document.head.outerHTML).toMatchSnapshot();
+    expect(document.body.outerHTML).toMatchSnapshot();
+
+    dispose();
+
+    // Everything should be cleared
+    expect(mounted).toStrictEqual(false);
+
+    expect(document.head.outerHTML).toMatchSnapshot();
+    expect(document.body.outerHTML).toMatchSnapshot();
+
+    target.remove();
 });

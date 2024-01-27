a8d5d5296ddcaf4cb55e8040e550c69d4f673e87
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2024-01-28 10:12:17 +1100

    ConstantEditor working

--- packages/sweeter-typeytypetype-web/src/editors/ConstantEditor.tsx
+++ packages/sweeter-typeytypetype-web/src/editors/ConstantEditor.tsx
@@ -1,6 +1,7 @@
 import { TextArea } from '@captainpants/sweeter-gummybear';
 import { type EditorProps } from '../types.js';
+import { $calc, $val } from '@captainpants/sweeter-core';
 
 export function ConstantEditor({ model }: Readonly<EditorProps>): JSX.Element {
-    return <TextArea disabled fillWidth readOnly value={String(model.value)} />;
+    return <TextArea disabled fillWidth readOnly value={$calc(() => String($val(model).value))} />;
 }

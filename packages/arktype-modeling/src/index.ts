46b1fea96bb176ac17dba4fa6ad9867bc7e7e998
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2024-11-27 22:00:00 +1100

    Removed reference to file previously deleted.

--- packages/arktype-modeling/src/index.ts
+++ packages/arktype-modeling/src/index.ts
@@ -1,7 +1,6 @@
 export * from './ContextualValues.js';
 export * from './matcher/index.js';
 export * from './models/index.js';
-export * from './testingTypes.js';
 export * from './types.js';
 export * from './utility/index.js';
 export * from './validation/index.js';

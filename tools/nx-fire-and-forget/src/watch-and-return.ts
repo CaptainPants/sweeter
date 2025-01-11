91f954212d3a8c763f7919937bdad267e9ebde09
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2025-01-12 10:47:14 +1100

    WIP...

--- tools/nx-fire-and-forget/src/watch-and-return.ts
+++ tools/nx-fire-and-forget/src/watch-and-return.ts
@@ -1,14 +1,31 @@
 import { PromiseExecutor } from "@nx/devkit";
-import { WatchAndReturnExecutorSchema } from "./schema";
+import { type WatchAndReturnExecutorSchema } from "./schema.js";
+
+import * as child_process from 'node:child_process';
+import { stderr, stdout } from "node:process";
 
 const runExecutor: PromiseExecutor<WatchAndReturnExecutorSchema> = async (
-  options,
+    options,
 ) => {
-    throw new Error("TEST");
-  console.log("Executor ran for WatchAndReturn", options);
-  return {
-    success: true,
-  };
+    return new Promise((resolve, reject) => {
+        console.log("Executor ran for WatchAndReturn", options);
+    
+        const child = child_process.spawn('pnpm.cmd run watch', { });
+        child.stderr.on('data', data => {
+            stderr.write(data);
+        });
+        child.stdout.on('data', data => {
+            console.warn(data, typeof data, data.constructor.name);
+    
+            stdout.write(data);
+        });
+        child.stdout.on('close', () => {
+            resolve({
+                success: true,
+            });
+        });
+        child.unref();
+    });
 };
 
 export default runExecutor;

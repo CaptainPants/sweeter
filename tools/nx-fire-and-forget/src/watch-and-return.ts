673a42310208acb85d855455224ad3c756ced135
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2025-01-15 22:34:36 +1100

    Well the nx-based solution isn't going to work as the parent process still doesn't exit properly.

--- tools/nx-fire-and-forget/src/watch-and-return.ts
+++ tools/nx-fire-and-forget/src/watch-and-return.ts
@@ -2,6 +2,23 @@ import { PromiseExecutor } from "@nx/devkit";
 import { type WatchAndReturnExecutorSchema } from "./schema.js";
 
 import * as child_process from 'node:child_process';
+import * as path from "node:path";
+import { stdout } from "node:process";
+import { Readable } from "node:stream"
+
+type StripAnsi = typeof import('strip-ansi').default
+type StripAnsiModule = { default: StripAnsi };
+
+/**
+ * https://gist.github.com/passbyval/b85f79381816c197c5c651b7c0b00d5e
+ * This is some hot garbage https://github.com/nrwl/nx/issues/15682
+ * @param moduleName 
+ * @returns 
+ */
+async function loadESMModule<ModuleType>(moduleName: string): Promise<ModuleType> {
+    const doImport = new Function('return import(' + JSON.stringify(moduleName) + ')') as unknown as () => Promise<ModuleType>;
+    return await doImport();
+}
 
 const runExecutor: PromiseExecutor<WatchAndReturnExecutorSchema> = async (
     options,
@@ -9,64 +26,95 @@ const runExecutor: PromiseExecutor<WatchAndReturnExecutorSchema> = async (
 ) => {
     const successPattern: RegExp | null = options.successPattern ? new RegExp(options.successPattern) : null;
 
-    return new Promise((resolve, reject) => {
-        console.log("Executor ran for WatchAndReturn", options, context.root);
-    
-        const child = child_process.spawn(options.command, { shell: true, cwd: context.cwd, stdio: ['inherit', 'pipe', 'inherit'] });
+    const stripAnsi = (await loadESMModule<{ default: StripAnsi }>('strip-ansi')).default;
 
-        child.stdout.setEncoding('utf-8');
-        const reader = createReader(line => {
-            console.warn('OUTPUT ', line)
+    const promise = new Promise<{ success: boolean }>((resolve) => {
+        const projectName = context.projectName;
+
+        const cwd = path.join(context.root, context.projectsConfigurations.projects[projectName].root);
+
+        console.log("Executor ran for WatchAndReturn", options, cwd);
+
+        const child = child_process.spawn(options.command, { shell: true, cwd: cwd, stdio: ['inherit', 'pipe', 'inherit'] });
+
+        function closeStdoutAndResolve() {
+            // Testing if this releases the parent process
+            console.error('Closing stdout pipe with child')
+            child.stdout.unpipe();
+            child.stdout.destroy();
+
+            resolve({
+                success: true
+            });
+        }
+
+        const { flush } = createPassthrough(child.stdout, stdout, line => {
             if (successPattern) {
+                // console.error('Testing ', JSON.stringify(line));
                 const match = line.match(successPattern);
                 if (match) {
-                    console.warn('MATCHED');
+                    console.error('MATCHED SUCCESS SEQUENCE! Resolving..');
+                    
+                    closeStdoutAndResolve();
                 }
             }
-        });
-        child.stdout.on('data', reader.read);
+        }, stripAnsi);
 
         child.addListener('close', () => {
-            reader.close();
-            resolve({
-                success: true,
-            });
+            flush();
+            closeStdoutAndResolve();
         })
         child.unref();
     });
+
+    const res = await promise;
+
+    console.error('FINISHED');
+
+    return res;
 };
 
-function createReader(callback: (line: string) => void) {
-    let buffer: string = '';
+function createPassthrough(source: Readable, target: NodeJS.WriteStream, callback: (line: string) => void, stripAnsi: StripAnsi) {
+    let buffer: string | null = '';
 
-    return {
-        read: (data: string) => {
-            console.warn(data);
+    function invokeCallback(line: string) {
+        // Write to target the unmodified data
+        target.write(line);
+        
+        const stripped = stripAnsi(line);
+        callback(stripped);
+    }
 
-            let remaining = data;
+    function listener(data: string) {
+        let remaining = data;
 
-            for (;;) {
-                let beforeFirstLineBreak: string;
-                [beforeFirstLineBreak, remaining] = removeFirstLine(remaining);
+        for (; ;) {
+            let beforeFirstLineBreak: string;
+            [beforeFirstLineBreak, remaining] = removeFirstLine(remaining);
 
-                buffer += beforeFirstLineBreak;
+            buffer = (buffer ?? '') + beforeFirstLineBreak;
 
-                if (remaining) {
-                    callback(beforeFirstLineBreak);
-                    beforeFirstLineBreak = '';
-                }
-                else {
-                    break; // there is nothing left, exit loop
-                }
+            if (remaining !== null) {
+                invokeCallback(beforeFirstLineBreak);
+                beforeFirstLineBreak = '';
             }
-        },
-        close: () => {
-            if (buffer){
-                callback(buffer);
-                buffer = '';
+            else {
+                break; // there is nothing left, exit loop
             }
         }
     };
+
+    source.setEncoding('utf-8');
+    source.addListener('data', listener);
+
+    function flush() {
+        if (buffer !== null) {
+            invokeCallback(buffer);
+            buffer = null;
+        }
+    }
+
+    return { flush: flush };
 }
 
 function removeFirstLine(data: string): [line: string, remainder: string | null] {

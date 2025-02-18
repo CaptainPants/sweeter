16f19041e5754bd0eeeec9eebfa1dbe384e5f393
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2025-02-18 22:57:24 +1100

    WIP

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,4 +1,4 @@
-import { execSync } from "child_process";
+import { execSync, spawn } from "child_process";
 import { exit } from "process";
 
 const args = process.argv.slice(2);
@@ -15,7 +15,7 @@ function output(text: string) {
     console.log(text);
 }
 
-function run(command: string, writeOutput = true): string {
+function runAndReturn(command: string, writeOutput = true): string {
     if (debug) {
         console.log('RUNNING ', command);
         return '';
@@ -27,30 +27,40 @@ function run(command: string, writeOutput = true): string {
     return res;
 }
 
-const currentBranch = run('git rev-parse --abbrev-ref HEAD', false);
+async function runAsyncWithStdoutPassthrough(command: string): Promise<void> {
+    return new Promise<void>(
+        (resolve, reject) => {
+            const proc = spawn(command, { stdio: "overlapped" });
+
+            proc.on('close', (code, signal) => code == 0 ? resolve(void 0) : reject(new Error(`Killed ${code} (${signal})`)));
+        }
+    )
+}
+
+const currentBranch = runAndReturn('git rev-parse --abbrev-ref HEAD', false);
 output(`Releasing based on branch ${currentBranch}.`);
 
-const diff = run('git diff --name-only --raw', false)?.trim();
+const diff = runAndReturn('git diff --name-only --raw', false)?.trim();
 const changedFiles = diff === '' ? [] : diff.split('\n');
 if (changedFiles.length > 0) {
     output(`Changes found, please commit or revert them to continue: \n- ${changedFiles.join('\n- ')}`);
     exit(0);
 }
 
-run('git branch temp_release -m -f');
+runAndReturn('git branch temp_release -m -f');
 
-run(`pnpm run set-versions ${ver}`);
+runAndReturn(`pnpm run set-versions ${ver}`);
 
-run('git add -A');
-run('git commit -m "Version numbers"');
+runAndReturn('git add -A');
+runAndReturn('git commit -m "Version numbers"');
 
 if (dryRun) {
     output('PUBLISHING (DRY RUN)');
-    run('pnpm run publish-all:dry-run');
+    await runAsyncWithStdoutPassthrough('pnpm run publish-all:dry-run');
 }
 else {
     output('PUBLISHING (REAL)');
-    run('pnpm run publish-all:dry-run');
+    await runAsyncWithStdoutPassthrough('pnpm run publish-all:dry-run');
 }
 
 exit(0);

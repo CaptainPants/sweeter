d0cddec357937d157b22f6293dd91525266564cf
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2025-02-19 23:17:21 +1100

    Rejigged into a method to make error cleanup work nicer.

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,16 +1,8 @@
 import { execSync, spawn } from "child_process";
-import { exit } from "process";
-
-const args = process.argv.slice(2);
-
-if (args.length < 1) {
-    throw new Error('Expected at least one argument');
-}
-
-const [ver, dryRun = true] = args;
+import { stdout } from "process";
 
 function output(text: string) {
-    console.log(text);
+    stdout.write(text);
 }
 
 function runAndReturn(command: string, { writeOutput = true, logCommand = false } = {}): string {
@@ -35,31 +27,48 @@ async function runAsyncWithStdoutPassthrough(command: string): Promise<void> {
     )
 }
 
-const currentBranch = runAndReturn('git rev-parse --abbrev-ref HEAD', { writeOutput: false });
-output(`Releasing based on branch ${currentBranch}.`);
+async function main(args: readonly string[]) {
+    if (args.length < 1) {
+        throw new Error('Expected at least one argument');
+    }
+    
+    const [ver, dryRun = true] = args;
 
-const diff = runAndReturn('git diff --name-only --raw', { writeOutput: false })?.trim();
-const changedFiles = diff === '' ? [] : diff.split('\n');
-if (changedFiles.length > 0) {
-    output(`Changes found, please commit or revert them to continue: \n- ${changedFiles.join('\n- ')}`);
-    exit(1);
+    const currentBranch = runAndReturn('git rev-parse --abbrev-ref HEAD', { writeOutput: false });
+    output(`Releasing based on branch ${currentBranch}.`);
+
+    const diff = runAndReturn('git diff --name-only --raw', { writeOutput: false })?.trim();
+    const changedFiles = diff === '' ? [] : diff.split('\n');
+    if (changedFiles.length > 0) {
+        const message = `Changes found, please commit or revert them to continue: \n- ${changedFiles.join('\n- ')}`;
+        output(message);
+        throw new Error(message);
+    }
+
+    try {
+        runAndReturn('git branch temp_release -c -f');
+        runAndReturn('git switch temp_release');
+
+        runAndReturn(`pnpm run set-versions ${ver}`);
+
+        runAndReturn('git add -A');
+        runAndReturn('git commit -m "Version numbers"');
+
+        if (dryRun) {
+            output('PUBLISHING (DRY RUN)');
+            await runAsyncWithStdoutPassthrough('pnpm run publish-all:dry-run');
+        }
+        else {
+            output('PUBLISHING (REAL)');
+            await runAsyncWithStdoutPassthrough('pnpm run publish-all:dry-run');
+        }
+    }
+    finally {
+        // Return to original branch
+        runAndReturn(`git switch ${currentBranch}`);
+    }
+
+    return;
 }
 
-runAndReturn('git branch temp_release -c -f');
-runAndReturn('git switch temp_release');
-
-runAndReturn(`pnpm run set-versions ${ver}`);
-
-runAndReturn('git add -A');
-runAndReturn('git commit -m "Version numbers"');
-
-if (dryRun) {
-    output('PUBLISHING (DRY RUN)');
-    await runAsyncWithStdoutPassthrough('pnpm run publish-all:dry-run');
-}
-else {
-    output('PUBLISHING (REAL)');
-    await runAsyncWithStdoutPassthrough('pnpm run publish-all:dry-run');
-}
-
-exit(0);
+await main(process.argv.slice(2));
